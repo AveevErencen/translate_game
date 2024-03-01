c29df42dcbdd927e1b9a6fc33cdf2da052358b10
«AndreyRSJS» <agaidarev@gmail.com>
2024-03-01 13:44:04 +0300

    gyyg

--- src/components/ui/OneTheme.jsx
+++ src/components/ui/OneTheme.jsx
@@ -3,29 +3,16 @@ import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
 export default function OneTheme({ themes }) {
-  const wordBoxStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '50px',
-  };
-
-  const oneCardStyle = {
-    display: 'flex',
-    justifyContent: 'flexStart',
-    flexDirection: 'column',
-    width: '18rem',
-  };
-
   const clickHandler = (id) => {
     window.location.href = `/cardpage/${id}`;
   };
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+    <div className="mainCardTheme ">
       {themes?.map((theme) => (
-        <Card key={theme.id} style={oneCardStyle}>
-          <Card.Body style={oneCardStyle}>
-            <div style={wordBoxStyle}>
+        <Card className="oneCarsStyle" key={theme.id}>
+          <Card.Body className="carsStyle">
+            <div className="wordBoxStyle">
               <Card.Title>{theme.theme_name}</Card.Title>
             </div>
             <Button
@@ -41,19 +28,4 @@ export default function OneTheme({ themes }) {
       ))}
     </div>
   );
-}
-
-{
-  /* <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-      {themes?.map((theme) => (
-        <Card key={theme.id} style={oneCardStyle}>
-          <Card.Body style={oneCardStyle}>
-            <div style={wordBoxStyle}>
-              <Card.Title>{theme.theme_name}</Card.Title>
-            </div>
-            <Button onClick={() => clickHandler(theme.id)} name="button_translate" type="button" variant="primary">Выбор</Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </div> */
 }
