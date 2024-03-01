c5b1e4d65532f0ffa1521a61ec54d211bf39f33f
AveevErencen <erenc@yandex.ru>
2024-03-01 13:28:03 +0300

    updateProfile

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -16,38 +16,3 @@ export default function NavBar({ user }) {
     </div>
   );
 }
-
-{ /* <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home" />
-        <a className="navbar-brand">EnglishLearningApp</a>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {user ? (
-              <Nav.Link style={navLinkStyle} href="/account">
-                {user.name}
-              </Nav.Link>
-            ) : (
-              <Nav.Link style={navLinkStyle} href="/auth/signin">
-                Войти
-              </Nav.Link>
-            )}
-            {user && (
-              <Nav.Link style={navLinkStyle} href="/themes">
-                Выбрать тему
-              </Nav.Link>
-            )}
-            {user ? (
-              <Nav.Link style={navLinkStyle} href="/auth/logout">
-                Выйти
-              </Nav.Link>
-            ) : (
-              <Nav.Link style={navLinkStyle} href="/auth/signup">
-                Зарегистрироваться
-              </Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> */ }
