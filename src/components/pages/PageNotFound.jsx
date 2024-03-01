196928bb373baf938eefd1b7916dc1348bd14bb1
mr.advert <mr.advert@yandex.ru>
2024-03-01 11:51:11 +0300

    notFoundPage fixed

--- src/components/pages/PageNotFound.jsx
+++ src/components/pages/PageNotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default function NoPageFound() {
+  const pageNotFoundStyle = {
+    width: '900px',
+  };
+
+  const divStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '100px',
+  };
+
   return (
-    <div>
-      <img src="https://spikelawrence.com/wp-content/uploads/404-screen.png" alt="pageNotFoundPic" />
+    <div style={divStyle}>
+      <img src="https://i.postimg.cc/Qd7qKV0t/5067423-middle.png" alt="pageNotFoundPic" style={pageNotFoundStyle} />
     </div>
   );
 }
