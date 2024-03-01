3db623cd58dbbef9f5a60c7686cc470224b35e0c
«AndreyRSJS» <agaidarev@gmail.com>
2024-03-01 11:01:28 +0300

    style ok

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar({ user }) {
     <div className="navbar">
       <div className="container">
         <div className="brand-name">
-          <a className="navbar-brand">English - Easy</a>
+          <a className="navbar-brand" href="/">English - Easy</a>
         </div>
         <div className="navbar-links">
           {user ? <a href="/account">{user.name}</a> : <a href="/auth/signin">Войти</a>}
@@ -16,38 +16,3 @@ export default function NavBar({ user }) {
     </div>
   );
 }
-
-{ /* <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home" />
-        <a className="navbar-brand">EnglishLearningApp</a>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {user ? (
-              <Nav.Link style={navLinkStyle} href="/account">
-                {user.name}
-              </Nav.Link>
-            ) : (
-              <Nav.Link style={navLinkStyle} href="/auth/signin">
-                Войти
-              </Nav.Link>
-            )}
-            {user && (
-              <Nav.Link style={navLinkStyle} href="/themes">
-                Выбрать тему
-              </Nav.Link>
-            )}
-            {user ? (
-              <Nav.Link style={navLinkStyle} href="/auth/logout">
-                Выйти
-              </Nav.Link>
-            ) : (
-              <Nav.Link style={navLinkStyle} href="/auth/signup">
-                Зарегистрироваться
-              </Nav.Link>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> */ }
